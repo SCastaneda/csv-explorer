0eaa64dc6dc5ce542463bf19f1d7fa68a5466b4a
Samuel Castaneda <sam@sam-the-man.com>
2017-05-28 00:37:11 -0400

    remove unused file

--- api/db/connect.js
+++
@@ -1,6 +0,0 @@
-var mongoose = require('mongoose');
-var mongoURI = process.env.MONGOHQ_URL || 'mongodb://localhost/csv';
-var db       = mongoose.connect(mongoURI);
-
-exports.schema   = mongoose.Schema;
-exports.mongoose = mongoose;
